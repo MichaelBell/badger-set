43be55dde055e583b46e998b798dca83e418b281
Mike Bell <mdb036@gmail.com>
2022-03-04 17:34:58 +0000

    Use ROSC and avoid even bringing up the external crystal

--- lpbadger/lpbadger.cpp
+++ lpbadger/lpbadger.cpp
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include "hardware/clocks.h"
 #include "hardware/regs/clocks.h"
+#include "hardware/watchdog.h"
 #include "hardware/pll.h"
 #include "hardware/irq.h"
 #include "hardware/flash.h"
@@ -8,54 +9,79 @@
 
 #include "lpbadger.hpp"
 
-void LowPowerBadger::init()
-{
-    // Configure all clocks to run off the USB PLL, and shut down
-  // the SYS PLL to save power.
-  // 24MHz seems to be the slowest we can run the system and still
-  // talk to the display - I'm not quite sure why it doesn't work
-  // with a slower system clock
-  clock_stop(clk_usb);
-  clock_stop(clk_adc);
-  clock_stop(clk_rtc);
+#define ROSC_MHZ 6
 
-  pll_init(pll_usb, 1, 480 * MHZ, 5, 4);
+extern "C" {
+void clocks_init()
+{
+    // Start tick in watchdog
+    watchdog_start_tick(ROSC_MHZ);
+
+    // Disable resus that may be enabled from previous software
+    clocks_hw->resus.ctrl = 0;
+
+    // Before we touch PLLs, switch sys and ref cleanly away from their aux sources.
+    hw_clear_bits(&clocks_hw->clk[clk_sys].ctrl, CLOCKS_CLK_SYS_CTRL_SRC_BITS);
+    while (clocks_hw->clk[clk_sys].selected != 0x1)
+        tight_loop_contents();
+    hw_clear_bits(&clocks_hw->clk[clk_ref].ctrl, CLOCKS_CLK_REF_CTRL_SRC_BITS);
+    while (clocks_hw->clk[clk_ref].selected != 0x1)
+        tight_loop_contents();
+
+    // Configure clocks
+    clock_configure(clk_ref,
+                    CLOCKS_CLK_REF_CTRL_SRC_VALUE_ROSC_CLKSRC_PH,
+                    0, // No aux mux
+                    ROSC_MHZ * MHZ,
+                    ROSC_MHZ * MHZ);
 
   clock_configure(clk_sys,
       CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
-      CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-      24 * MHZ,
-      24 * MHZ);
+      CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_ROSC_CLKSRC,
+      ROSC_MHZ * MHZ,
+      ROSC_MHZ * MHZ);
+
+  // Must configure USB clock to allow init to complete.
+  clock_configure(clk_usb,
+      0,
+      CLOCKS_CLK_USB_CTRL_AUXSRC_VALUE_ROSC_CLKSRC_PH,
+      ROSC_MHZ * MHZ,
+      ROSC_MHZ * MHZ);
 
   clock_configure(clk_peri,
       0,
-      CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-      24 * MHZ,
-      24 * MHZ);
+      CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_ROSC_CLKSRC_PH,
+      ROSC_MHZ * MHZ,
+      ROSC_MHZ * MHZ);
 
   clock_configure(clk_adc,
       0,
-      CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-      24 * MHZ,
-      24 * MHZ);
+      CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_ROSC_CLKSRC_PH,
+      ROSC_MHZ * MHZ,
+      ROSC_MHZ * MHZ);
 
   clock_configure(clk_rtc,
       0,
-      CLOCKS_CLK_RTC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-      24 * MHZ,
+      CLOCKS_CLK_RTC_CTRL_AUXSRC_VALUE_ROSC_CLKSRC_PH,
+      ROSC_MHZ * MHZ,
       46875);
 
+  // The PLLs shouldn't be initialized, but this does no harm
   pll_deinit(pll_sys);
+  pll_deinit(pll_usb);
+}
+}
 
-  // Disable unused clock regions
-  clocks_hw->wake_en1 = 0x703f;
-  clocks_hw->wake_en0 = 0xf3ef8f3f;
+void LowPowerBadger::init()
+{
+  // Can now stop the USB clock
+  clock_stop(clk_usb);
 
-  sleep_ms(1);
+  // And disable unused clock regions
+  clocks_hw->wake_en1 = 0x303f;
+  clocks_hw->wake_en0 = 0xf3ef0f3f;
 
   pimoroni::Badger2040::init();
-
-  sleep_ms(1);
 }
 
 #define FLASH_TARGET_OFFSET 0x100000
